0f25f8661cf2678dec84c04794feed594a8f5991
mpanarin <m.panarin@mobilunity.com>
2018-08-01 23:55:18 +0300

    [MIG] purchase_all_shipments: migrate to v11

---
+++ purchase_all_shipments/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Purchase All Shipments",
+    "version": "11.0.1.0.0",
+    "author": "Camptocamp,"
+              "Odoo Community Association (OCA),",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "view/purchase_order.xml",
+    ],
+}
