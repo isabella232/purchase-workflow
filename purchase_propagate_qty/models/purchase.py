b3f8d5da19bab5b5032a94b32c0799d38350ce99
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-04-30 15:27:10 +0200

    [ADD] purchase_propagate_qty
    This module propagates qty decrease on purchase lines to the stock.moves

---
+++ purchase_propagate_qty/models/purchase.py
@@ -0,0 +1,28 @@
+# Copyright 2014-2016 Numérigraphe SARL
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api
+from odoo.tools import float_is_zero
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.multi
+    def write(self, values):
+        res = super().write(values)
+        for line in self:
+            if line.state != 'purchase':
+                continue
+            for move in line.move_dest_ids | line.move_ids:
+                if move.state in ('cancel', 'done'):
+                    continue
+                if move.product_id != line.product_id:
+                    continue
+                move.product_uom_qty = line.product_uom_qty
+                if float_is_zero(
+                        move.product_uom_qty, line.product_uom.rounding
+                ):
+                    move._action_cancel()
+        return res
