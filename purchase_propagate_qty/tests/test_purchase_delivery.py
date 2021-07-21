488e6685398f43d582f3f0c6b27335b0429978bb
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-07-21 13:21:33 +0200

    [13.0] purchase_propagate_qty: handle splitted moves

--- purchase_propagate_qty/tests/test_purchase_delivery.py
+++ purchase_propagate_qty/tests/test_purchase_delivery.py
@@ -94,3 +94,57 @@ class TestQtyUpdate(SavepointCase):
         line1.write({"product_qty": 0})
         self.assertEqual(move1.product_uom_qty, 0)
         self.assertEqual(move1.state, "cancel")
+
+    def test_purchase_line_splitted_move(self):
+        # Add qty, create a new move with the diff
+        line1 = self.po.order_line[0]
+        move1 = self.env["stock.move"].search([("purchase_line_id", "=", line1.id)])
+        self.assertEqual(len(move1), 1)
+        line1.write({"product_qty": 64})
+        self.assertEqual(move1.product_uom_qty, 64)
+        # split the move
+        move1._split(22)
+        moves = self.env["stock.move"].search([("purchase_line_id", "=", line1.id)])
+        self.assertEqual(len(moves), 2)
+        self.assertEqual(moves.mapped("product_uom_qty"), [42.0, 22.0])
+        # Then, decrease 11
+        line1.write({"product_qty": 53})
+        self.assertEqual(moves.mapped("product_uom_qty"), [42.0, 11.0])
+        # Again, decrease 11
+        line1.write({"product_qty": 42})
+        self.assertEqual(moves.mapped("product_uom_qty"), [42.0, 0.0])
+        active_move = moves.filtered(lambda m: m.state != "cancel")
+        inactive_move = moves.filtered(lambda m: m.state == "cancel")
+        self.assertEqual(active_move.product_uom_qty, 42)
+        self.assertEqual(inactive_move.product_uom_qty, 0)
+
+    def test_purchase_line_splitted_move_w_reserved_qty(self):
+        # Add qty, create a new move with the diff
+        line1 = self.po.order_line[0]
+        move1 = self.env["stock.move"].search([("purchase_line_id", "=", line1.id)])
+        self.assertEqual(len(move1), 1)
+        line1.with_context(unittests=True).write({"product_qty": 64})
+        self.assertEqual(move1.product_uom_qty, 64)
+        # split the move
+        move1._split(22)
+        moves = self.env["stock.move"].search([("purchase_line_id", "=", line1.id)])
+        move2 = moves - move1
+        # reserve 11 qty
+        self.env["stock.move.line"].create(
+            dict(move2._prepare_move_line_vals(), qty_done=11)
+        )
+        self.assertEqual(move2.quantity_done, 11)
+        line1.write({"product_qty": 60})
+        self.assertEqual(moves.mapped("product_uom_qty"), [42.0, 18.0])
+        # We cannot remove more than 11 on move2, deduce the max from it,
+        # then deduce from move1
+        line1.write({"product_qty": 50})
+        self.assertEqual(moves.mapped("product_uom_qty"), [39.0, 11.0])
+        # We should not be able to set a qty < to 11, since 11 products are reserved
+        with self.assertRaises(UserError):
+            line1.write({"product_qty": 5})
+        # with 11, move1 should be cancelled
+        line1.write({"product_qty": 11})
+        self.assertEqual(move1.product_uom_qty, 0.0)
+        self.assertEqual(move1.state, "cancel")
+        self.assertEqual(move2.product_uom_qty, 11.0)
