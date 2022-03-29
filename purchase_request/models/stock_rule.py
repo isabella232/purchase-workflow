5b9807e83b9dce9a6650a7ae687d86a2d030cd5f
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2022-03-29 14:05:36 +0200

    fixup! fixup! [IMP] do not create multiple request lines for same product and day

--- purchase_request/models/stock_rule.py
+++ purchase_request/models/stock_rule.py
@@ -138,6 +138,11 @@ class StockRule(models.Model):
             [
                 ("product_id", "=", request_line_data["product_id"]),
                 ("date_required", "=", request_line_data["date_required"].date()),
+                (
+                    "purchase_state",
+                    "=",
+                    False,
+                ),  # avoid updating if there is RFQ or PO linked
             ],
         )
         if same_product_date_request_line:
